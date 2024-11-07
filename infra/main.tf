6bb7fa21e396e627081456db0a5ace3e45fa07a8
Livin <livinmathew99@gmail.com>
2024-11-07 21:48:11 +0530

    Update infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
 variable "aws_access_key" {
   type = string
 }
@@ -6,11 +12,10 @@ variable "aws_secret_key" {
   type = string
 }
 
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
-  default     = "us-east-1"
-}
+# variable "aws_profile" {
+#   type        = string
+#   description = "AWS profile"
+# }
 
 variable "environment" {
   type        = string
@@ -21,11 +26,11 @@ variable "environment" {
 variable "project_name" {
   type        = string
   description = "Project name"
-  default     = "hello-world-5963881"
+  default     = "pojlkasndfj-1982030"
 }
 
 provider "aws" {
-  region     = var.aws_region
+  region  = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   default_tags {
@@ -35,116 +40,117 @@ provider "aws" {
     }
   }
 }
+  
 
-resource "aws_apigatewayv2_api" "aws-api-gateway-iqaze6zu" {
-  name          = "hello-world-api"
-  protocol_type = "HTTP"
+resource "aws_apigatewayv2_api" "aws-api-gateway-l3xiygho" {
+    name          = "pok-gateway"
+    protocol_type = "HTTP"
 }
 
-resource "aws_cloudwatch_log_group" "aws-api-gateway-iqaze6zu-log-group" {
-  name              = "/aws/apigateway/aws-api-gateway-iqaze6zu"
-  retention_in_days = 7
+resource "aws_cloudwatch_log_group" "aws-api-gateway-l3xiygho-log-group" {
+    name              = "/aws/apigateway/aws-api-gateway-l3xiygho"
+    retention_in_days = 7
 }
 
-resource "aws_apigatewayv2_stage" "aws-api-gateway-iqaze6zu-stage" {
-  api_id      = aws_apigatewayv2_api.aws-api-gateway-iqaze6zu.id
-  name        = "dev"
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.aws-api-gateway-iqaze6zu-log-group.arn
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-    })
-  }
+resource "aws_apigatewayv2_stage" "aws-api-gateway-l3xiygho-stage" {
+    api_id      = aws_apigatewayv2_api.aws-api-gateway-l3xiygho.id
+    name        = "dev"
+    auto_deploy = true
+    
+    access_log_settings {
+        destination_arn = aws_cloudwatch_log_group.aws-api-gateway-l3xiygho-log-group.arn
+        format = jsonencode({
+            requestId               = "$context.requestId"
+            sourceIp                = "$context.identity.sourceIp"
+            requestTime             = "$context.requestTime"
+            protocol                = "$context.protocol"
+            httpMethod              = "$context.httpMethod"
+            resourcePath            = "$context.resourcePath"
+            routeKey                = "$context.routeKey"
+            status                  = "$context.status"
+            responseLength          = "$context.responseLength"
+            integrationErrorMessage = "$context.integrationErrorMessage"
+        })
+    }
 }
 
 output "stage_endpoint" {
-  value = aws_apigatewayv2_stage.aws-api-gateway-iqaze6zu-stage.invoke_url
+    value = aws_apigatewayv2_stage.aws-api-gateway-l3xiygho-stage.invoke_url
 }
 
-resource "aws_apigatewayv2_integration" "aws-api-gateway-iqaze6zu-to-aws-lambda-n70ol2gt-integration" {
-  api_id           = aws_apigatewayv2_api.aws-api-gateway-iqaze6zu.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.aws-lambda-n70ol2gt.invoke_arn
+resource "aws_apigatewayv2_integration" "aws-api-gateway-l3xiygho-to-aws-lambda-masxovpv-integration" {
+    api_id           = aws_apigatewayv2_api.aws-api-gateway-l3xiygho.id
+    integration_type = "AWS_PROXY"
+    integration_uri  = aws_lambda_function.aws-lambda-masxovpv.invoke_arn
 }
 
-resource "aws_apigatewayv2_route" "aws-api-gateway-iqaze6zu-to-aws-lambda-n70ol2gt-route" {
-  api_id    = aws_apigatewayv2_api.aws-api-gateway-iqaze6zu.id
-  route_key = "GET /"
-  target    = "integrations/${aws_apigatewayv2_integration.aws-api-gateway-iqaze6zu-to-aws-lambda-n70ol2gt-integration.id}"
+resource "aws_apigatewayv2_route" "aws-api-gateway-l3xiygho-to-aws-lambda-masxovpv-route" {
+    api_id    = aws_apigatewayv2_api.aws-api-gateway-l3xiygho.id
+    route_key = "GET /"
+    target    = "integrations/${aws_apigatewayv2_integration.aws-api-gateway-l3xiygho-to-aws-lambda-masxovpv-integration.id}"
 }
-
-resource "aws_lambda_permission" "aws-api-gateway-iqaze6zu-to-aws-lambda-n70ol2gt-permission" {
-  statement_id  = "aws-api-gateway-iqaze6zu-to-aws-lambda-n70ol2gt-permission"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.aws-lambda-n70ol2gt.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.aws-api-gateway-iqaze6zu.execution_arn}/*/*"
-}
-
-
-data "archive_file" "aws-lambda-n70ol2gt-archive" {
-  type        = "zip"
-  source_dir  = "../src"
-  output_path = ".output/aws-lambda-n70ol2gt-code.zip"
+    
+resource "aws_lambda_permission" "aws-api-gateway-l3xiygho-to-aws-lambda-masxovpv-permission" {
+    statement_id  = "aws-api-gateway-l3xiygho-to-aws-lambda-masxovpv-permission"
+    action        = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.aws-lambda-masxovpv.function_name
+    principal     = "apigateway.amazonaws.com"
+    source_arn    = "${aws_apigatewayv2_api.aws-api-gateway-l3xiygho.execution_arn}/*/*"
 }
 
 
-resource "aws_cloudwatch_log_group" "aws-lambda-n70ol2gt-log-group" {
-  name              = "/aws/lambda/aws-lambda-n70ol2gt"
-  retention_in_days = 7
+data "archive_file" "aws-lambda-masxovpv-archive" {
+    type        = "zip"
+    source_dir  = "../src"
+    output_path = ".output/aws-lambda-masxovpv-code.zip"
 }
 
 
-resource "aws_iam_role" "aws-lambda-n70ol2gt-role" {
-  name = "aws-lambda-n70ol2gt-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
+resource "aws_cloudwatch_log_group" "aws-lambda-masxovpv-log-group" {
+    name              = "/aws/lambda/aws-lambda-masxovpv"
+    retention_in_days = 7
 }
 
 
-resource "aws_iam_role_policy_attachment" "aws-lambda-n70ol2gt-policy-attachment" {
-  role       = aws_iam_role.aws-lambda-n70ol2gt-role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_role" "aws-lambda-masxovpv-role" {
+    name = "aws-lambda-masxovpv-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+            },
+        ]
+    })
 }
 
 
-resource "aws_lambda_function" "aws-lambda-n70ol2gt" {
-  function_name    = "aws-lambda-n70ol2gt"
-  role             = aws_iam_role.aws-lambda-n70ol2gt-role.arn
-  handler          = "index.handler"
-  runtime          = "nodejs20.x"
-  timeout          = 30
-  memory_size      = 128
-  filename         = data.archive_file.aws-lambda-n70ol2gt-archive.output_path
-  source_code_hash = data.archive_file.aws-lambda-n70ol2gt-archive.output_base64sha256
-  logging_config {
-    log_group  = aws_cloudwatch_log_group.aws-lambda-n70ol2gt-log-group.name
-    log_format = "JSON"
-  }
+resource "aws_iam_role_policy_attachment" "aws-lambda-masxovpv-policy-attachment" {
+    role       = aws_iam_role.aws-lambda-masxovpv-role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-  environment {
-    variables = {}
-  }
+
+resource "aws_lambda_function" "aws-lambda-masxovpv" {
+    function_name    = "aws-lambda-masxovpv"
+    role             = aws_iam_role.aws-lambda-masxovpv-role.arn
+    handler          = "index.handler"
+    runtime          = "nodejs20.x"
+    timeout          = 30
+    memory_size      = 128
+    filename         = data.archive_file.aws-lambda-masxovpv-archive.output_path
+    source_code_hash = data.archive_file.aws-lambda-masxovpv-archive.output_base64sha256
+    logging_config {
+        log_group  = aws_cloudwatch_log_group.aws-lambda-masxovpv-log-group.name
+        log_format = "JSON"
+    }
+
+    environment {
+        variables = {}
+    }
 }
