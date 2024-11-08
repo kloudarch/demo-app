f7d2ee02d9c41b0ed236edf6c2df3f3845a4f02e
Livin <livinmathew99@gmail.com>
2024-11-08 14:37:12 +0530

    Update infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -26,7 +26,7 @@ variable "environment" {
 variable "project_name" {
   type        = string
   description = "Project name"
-  default     = "hello-world-5963881"
+  default     = "hello-world2-6140098"
 }
 
 provider "aws" {
@@ -41,3 +41,116 @@ provider "aws" {
   }
 }
   
+
+resource "aws_apigatewayv2_api" "aws-api-gateway-ft082qjp" {
+    name          = "hello-gateway"
+    protocol_type = "HTTP"
+}
+
+resource "aws_cloudwatch_log_group" "aws-api-gateway-ft082qjp-log-group" {
+    name              = "/aws/apigateway/aws-api-gateway-ft082qjp"
+    retention_in_days = 1
+}
+
+resource "aws_apigatewayv2_stage" "aws-api-gateway-ft082qjp-stage" {
+    api_id      = aws_apigatewayv2_api.aws-api-gateway-ft082qjp.id
+    name        = "dev"
+    auto_deploy = true
+    
+    access_log_settings {
+        destination_arn = aws_cloudwatch_log_group.aws-api-gateway-ft082qjp-log-group.arn
+        format = jsonencode({
+            requestId               = "$context.requestId"
+            sourceIp                = "$context.identity.sourceIp"
+            requestTime             = "$context.requestTime"
+            protocol                = "$context.protocol"
+            httpMethod              = "$context.httpMethod"
+            resourcePath            = "$context.resourcePath"
+            routeKey                = "$context.routeKey"
+            status                  = "$context.status"
+            responseLength          = "$context.responseLength"
+            integrationErrorMessage = "$context.integrationErrorMessage"
+        })
+    }
+}
+
+output "stage_endpoint" {
+    value = aws_apigatewayv2_stage.aws-api-gateway-ft082qjp-stage.invoke_url
+}
+
+resource "aws_apigatewayv2_integration" "aws-api-gateway-ft082qjp-to-aws-lambda-jin0vwrq-integration" {
+    api_id           = aws_apigatewayv2_api.aws-api-gateway-ft082qjp.id
+    integration_type = "AWS_PROXY"
+    integration_uri  = aws_lambda_function.aws-lambda-jin0vwrq.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "aws-api-gateway-ft082qjp-to-aws-lambda-jin0vwrq-route" {
+    api_id    = aws_apigatewayv2_api.aws-api-gateway-ft082qjp.id
+    route_key = "GET /hello"
+    target    = "integrations/${aws_apigatewayv2_integration.aws-api-gateway-ft082qjp-to-aws-lambda-jin0vwrq-integration.id}"
+}
+    
+resource "aws_lambda_permission" "aws-api-gateway-ft082qjp-to-aws-lambda-jin0vwrq-permission" {
+    statement_id  = "aws-api-gateway-ft082qjp-to-aws-lambda-jin0vwrq-permission"
+    action        = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.aws-lambda-jin0vwrq.function_name
+    principal     = "apigateway.amazonaws.com"
+    source_arn    = "${aws_apigatewayv2_api.aws-api-gateway-ft082qjp.execution_arn}/*/*"
+}
+
+
+data "archive_file" "aws-lambda-jin0vwrq-archive" {
+    type        = "zip"
+    source_dir  = "../src"
+    output_path = ".output/aws-lambda-jin0vwrq-code.zip"
+}
+
+
+resource "aws_cloudwatch_log_group" "aws-lambda-jin0vwrq-log-group" {
+    name              = "/aws/lambda/aws-lambda-jin0vwrq"
+    retention_in_days = 1
+}
+
+
+resource "aws_iam_role" "aws-lambda-jin0vwrq-role" {
+    name = "aws-lambda-jin0vwrq-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    Service = "lambda.amazonaws.com"
+                }
+            },
+        ]
+    })
+}
+
+
+resource "aws_iam_role_policy_attachment" "aws-lambda-jin0vwrq-policy-attachment" {
+    role       = aws_iam_role.aws-lambda-jin0vwrq-role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
+resource "aws_lambda_function" "aws-lambda-jin0vwrq" {
+    function_name    = "aws-lambda-jin0vwrq"
+    role             = aws_iam_role.aws-lambda-jin0vwrq-role.arn
+    handler          = "index.handler"
+    runtime          = "nodejs20.x"
+    timeout          = 30
+    memory_size      = 128
+    filename         = data.archive_file.aws-lambda-jin0vwrq-archive.output_path
+    source_code_hash = data.archive_file.aws-lambda-jin0vwrq-archive.output_base64sha256
+    logging_config {
+        log_group  = aws_cloudwatch_log_group.aws-lambda-jin0vwrq-log-group.name
+        log_format = "JSON"
+    }
+
+    environment {
+        variables = {}
+    }
+}
